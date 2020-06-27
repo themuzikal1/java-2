72cffb329c91525395d15087164f4e7f2887a36e
Korey Brooks <korey.brooks@mail.com>
2020-06-26 20:47:59 -0500

    updated exercise 1

--- burger_shop/Main.java
+++ burger_shop/Main.java
@@ -3,5 +3,7 @@ package burger_shop;
 public class Main {
     public static void main(String[] args) {
         //main execution of the burger shop goes here.  This is the "entry point" of your application
+    burger_shop.Burger burger = new burger_shop.Burger("Basic", "Sausage", 3.56, "White");
+    double price = burger.itemizedHamburger();
     }
 }
